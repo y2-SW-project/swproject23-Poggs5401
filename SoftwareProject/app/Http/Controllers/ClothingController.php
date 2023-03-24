3619061cfb55bbb81227b1b1790b0b77b1d23e68
Ben Sharkey (Student) <N00212320@iadt.ie>
2023-03-24 10:55:33 +0000

    Clothing Update Completed

--- SoftwareProject/app/Http/Controllers/ClothingController.php
+++ SoftwareProject/app/Http/Controllers/ClothingController.php
@@ -45,7 +45,7 @@ class ClothingController extends Controller
         $clothing_image = $request->file('clothing_image');
         $extension = $clothing_image->getClientOriginalExtension();
         // the filename needs to be unique, I use title and add the date to guarantee a unique filename, ISBN would be better here.
-        $filename = date('Y-m-d-His') . '_' . $request->input('title') . '.'. $extension;
+        $filename = date('Y-m-d-His') . '_' . $request->input('title') . '.' . $extension;
 
         // store the file $book_image in /public/images, and name it $filename
         $path = $clothing_image->storeAs('public/images', $filename);
@@ -75,12 +75,12 @@ class ClothingController extends Controller
      */
     public function show(Clothing $clothing)
     {
-        if(!Auth::id()) {
+        if (!Auth::id()) {
             return abort(403);
-          }
- 
-         //this function is used to get a book by the ID.
-         return view('clothing.show')->with('clothing', $clothing);
+        }
+
+        //this function is used to get a book by the ID.
+        return view('clothing.show')->with('clothing', $clothing);
     }
 
     /**
@@ -89,9 +89,9 @@ class ClothingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Clothing $clothing)
     {
-        //
+        return view('clothing.edit')->with('clothing', $clothing);
     }
 
     /**
@@ -101,9 +101,31 @@ class ClothingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Clothing $clothing)
     {
-        //
+
+
+        $request->validate([
+            'title' => 'required|max:120',
+            'description' => 'required|max:500',
+            'price' => 'required',
+            'clothing_image' => 'file|image'
+        ]);
+
+        $clothing_image = $request->file('clothing_image');
+        $extension = $clothing_image->getClientOriginalExtension();
+        $filename = date('Y-m-d-His') . '_' . $request->input('title') . '.' . $extension;
+
+        $path = $clothing_image->storeAs('public/images', $filename);
+
+        $clothing->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'clothing_image' => $filename
+        ]);
+
+        return to_route('clothing.show', $clothing)->with('success', 'Clothing Updated Successfully!');
     }
 
     /**
